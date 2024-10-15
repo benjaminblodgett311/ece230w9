709cdc4d59e7d970781b2def933a2f38f95567f7
ben256dev <benjamin@ben256.com>
2024-10-15 09:47:05 -0600

    remove dmux html crap

--- demultiplexer.v
+++ demultiplexer.v
@@ -7,9 +7,9 @@ module demultiplexer(
     output reg [3:0] D
 );
 
-    always @(*) begin <i class="conum" data-value="1"></i><b>(1)</b>
+    always @(*) begin
         case(sel)
-            2'b00: {D, C, B, A} &lt;= {4'b0, 4'b0, 4'b0, data}; <i class="conum" data-value="2"></i><b>(2)</b>
+            2'b00: {D, C, B, A} &lt;= {4'b0, 4'b0, 4'b0, data};
             2'b01: {D, C, B, A} &lt;= {4'b0, 4'b0, data, 4'b0};
             2'b10: {D, C, B, A} &lt;= {4'b0, data, 4'b0, 4'b0};
             2'b11: {D, C, B, A} &lt;= {data, 4'b0, 4'b0, 4'b0};
