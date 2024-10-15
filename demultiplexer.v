836212a38b0f024582a401c540594db495149969
ben256dev <benjamin@ben256.com>
2024-10-15 09:58:33 -0600

    demux conflict

--- demultiplexer.v
+++ demultiplexer.v
@@ -9,31 +9,11 @@ module demultiplexer(
 
     always @(*) begin
         case(sel)
-            2'b00: {D, C, B, A} <= {4'b0, 4'b0, 4'b0, data};
-            2'b01: {D, C, B, A} <= {4'b0, 4'b0, data, 4'b0};
-            2'b10: {D, C, B, A} <= {4'b0, data, 4'b0, 4'b0};
-            2'b11: {D, C, B, A} <= {data, 4'b0, 4'b0, 4'b0};
+            2'b00: {D, C, B, A} <= {8'b0, 8'b0, 8'b0, data};
+            2'b01: {D, C, B, A} <= {8'b0, 8'b0, data, 8'b0};
+            2'b10: {D, C, B, A} <= {8'b0, data, 8'b0, 8'b0};
+            2'b11: {D, C, B, A} <= {data, 8'b0, 8'b0, 8'b0};
         endcase
     end
 
 endmodule
-
-
-module internet_demux(
-    input [3:0] muxOutput,
-    input Enable,
-    input [1:0] Sel,
-    output [3:0] Lib, FD, School, Ribs
-);
-
-
-
-assign Lib =  (Enable && (Sel == 2'b00)) ? muxOutput : 4'b0000;
-assign FD = (Enable && (Sel == 2'b01)) ? muxOutput : 4'b0000;
-assign School = (Enable && (Sel == 2'b10)) ? muxOutput : 4'b0000;
-assign Ribs = (Enable && (Sel == 2'b11)) ? muxOutput : 4'b0000 ;
-
-
-    
-
-endmodule 
