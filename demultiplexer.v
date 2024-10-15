6e84d432ef73477a7f076aa612a6a7144424a0f3
Your Name <hannahszychulda@u.boisestate.edu>
2024-10-15 09:56:00 -0600

    working

--- demultiplexer.v
+++ demultiplexer.v
@@ -9,10 +9,10 @@ module demultiplexer(
 
     always @(*) begin
         case(sel)
-            2'b00: {D, C, B, A} &lt;= {4'b0, 4'b0, 4'b0, data};
-            2'b01: {D, C, B, A} &lt;= {4'b0, 4'b0, data, 4'b0};
-            2'b10: {D, C, B, A} &lt;= {4'b0, data, 4'b0, 4'b0};
-            2'b11: {D, C, B, A} &lt;= {data, 4'b0, 4'b0, 4'b0};
+            2'b00: {D, C, B, A} <= {4'b0, 4'b0, 4'b0, data};
+            2'b01: {D, C, B, A} <= {4'b0, 4'b0, data, 4'b0};
+            2'b10: {D, C, B, A} <= {4'b0, data, 4'b0, 4'b0};
+            2'b11: {D, C, B, A} <= {data, 4'b0, 4'b0, 4'b0};
         endcase
     end
 
